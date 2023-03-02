61247a0eba8d2078d900d31650b3adbd7d1027b7
Marieta <marieta.sahakyan.8@gmail.com>
2023-03-02 20:21:32 +0400

    Setup PostgreSQL and Database Migrations

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 
 const config = {
     PORT: process.env.PORT || 3001,
+    DATABASE: process.env.DB_NAME || '',
+    DB_HOST: process.env.HOST || '',
+    DB_USERNAME: process.env.DB_USERNAME || '',
+    DB_PASSWORD: process.env.DB_PASSWORD || '',
+    dialect: 'postgres'
 };
 
 Object.freeze(config);
---
+++ src/db/seeders/connection.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+const config = require('../../config/config');
+const sequelize = new Sequelize(config.DATABASE, config.DB_USERNAME, config.DB_PASSWORD, {
+    host: config.DB_HOST,
+    dialect: config.dialect
+});
+
+module.exports = sequelize;
